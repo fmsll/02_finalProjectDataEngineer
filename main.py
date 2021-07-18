132675c16e4963a7dfc1f49aa636e478a655a3b2
fmsll <fmsll@protonmail.com>
2021-07-18 13:41:35 -0300

    Leitura da tabela principal

--- main.py
+++ main.py
@@ -3,16 +3,22 @@
 from pyspark.sql import *
 from pyspark.conf import SparkConf
 from time import sleep
+from datetime import date
 
+# Variáveis Obrigatórias
 
+# Inicializando Spark Session com suporte ao Hive
 spark = (SparkSession
          .builder.appName("ProjetoFinal")
          .config("hive.metastore.uris", "thrift://localhost:10000", conf=SparkConf())
          .enableHiveSupport()
          .getOrCreate()
          )
+# Identificando o dia atual e fazendo um cast para str
+today = str(date.today())
+# Leitura da tabela principal
+raw_table = spark.sql("select * from covid19.covid19_particionada_estado")
 
-spark.sql("show databases").show()
 
 sleep(5)
 
